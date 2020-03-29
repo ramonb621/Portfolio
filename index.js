e63db1077f0fd4d7b882581d42521db4fd53a69f
Ramon Barajas <ramonb621@yahoo.com>
2020-03-28 21:07:50 -0700

    added styling to home page

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+let data = [
+    {
+        "name": "AppRx",
+        "src" : "images/apprx.png",
+        "href" : "https://apprx.herokuapp.com/"
+    },
+    {
+        "name" : "Toby",
+        "src" : "images/toby.png",
+        "href" : "https://toby-mgmt.herokuapp.com/"
+    },
+    {
+        "name" : "HelpFull",
+        "src" : "images/Helpfull.png",
+        "href" : "https://mkdr-project2.herokuapp.com/"
+    }
+]
 $(document).ready(function(){
     $("#carouselCaptions").carousel({
         interval:5000
     });
+
 })
